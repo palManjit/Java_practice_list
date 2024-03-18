4e379f6d572f1374bf1c96e821a444a133f5b98a
Manjit Pal <155907446+palManjit@users.noreply.github.com>
2024-03-18 11:45:57 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,16 @@
+//print sum of n natural numbers
+class Recursion{
+	public static void printNum(int i,int n,int sum){
+		if(i==n){
+			sum=sum+i;
+			System.out.println(sum);
+			return;
+		}
+			sum=sum+i;
+			printNum(i+1,n,sum);
+		
+	}
+	public static void main(String args[]){
+		printNum(1,5,0);
+	}
+}
